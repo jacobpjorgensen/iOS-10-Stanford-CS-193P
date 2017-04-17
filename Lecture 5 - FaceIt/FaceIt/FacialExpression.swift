c744b8a348d80c3d0242910c93c8df82ab1722df
Jacob Jorgensen <jacob.jorgensen@domo.com>
2017-04-17 14:03:56 -0600

    Added lecture 5 demo code

---
+++ Lecture 5 - FaceIt/FaceIt/FacialExpression.swift
@@ -0,0 +1,50 @@
+//
+//  FacialExpression.swift
+//  FaceIt
+//
+//  Created by Jacob Jorgensen on 4/17/17.
+//  Copyright © 2017 Domo, Inc. All rights reserved.
+//
+
+import Foundation
+
+// UI-independent representation of a facial expression
+
+struct FacialExpression {
+    
+    enum Eyes: Int {
+        case open
+        case closed
+        case squinting
+    }
+    
+    enum Mouth: Int {
+        case frown
+        case smirk
+        case neutral
+        case grin
+        case smile
+        
+        var sadder: Mouth {
+            return Mouth(rawValue: rawValue - 1) ?? .frown
+        }
+        
+        var happier: Mouth {
+            return Mouth(rawValue: rawValue + 1) ?? .smile
+        }
+    }
+    
+    var sadder: FacialExpression {
+        return FacialExpression(eyes: self.eyes, mouth: self.mouth.sadder)
+    }
+    
+    var happier: FacialExpression {
+        return FacialExpression(eyes: self.eyes, mouth: self.mouth.happier)
+    }
+    
+    let eyes: Eyes
+    let mouth: Mouth
+    
+}
+
+
