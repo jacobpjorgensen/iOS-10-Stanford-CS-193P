0381e40fa2885ad86fce91b708d0987bced14926
Jacob Jorgensen <jorgensendeveloper@gmail.com>
2017-08-29 07:13:48 -0600

    Updated Lecture 9 files

--- Lecture 9 - Smashtag/Smashtag/TweetTableViewController.swift
+++ Lecture 9 - Smashtag/Smashtag/TweetTableViewController.swift
@@ -21,6 +21,7 @@ class TweetTableViewController: UITableViewController, UITextFieldDelegate {
         didSet {
             searchTextField?.text = searchText
             searchTextField?.resignFirstResponder()
+            lastTwitterRequest = nil
             tweets.removeAll()
             tableView.reloadData()
             searchForTweets()
@@ -30,7 +31,7 @@ class TweetTableViewController: UITableViewController, UITextFieldDelegate {
     
     private func twitterRequest() -> Twitter.Request? {
         if let query = searchText, !query.isEmpty {
-            return Twitter.Request(search: query, count: 100)
+            return Twitter.Request(search: "\(query) - filter:safe -filter:retweets", count: 100)
         }
         return nil
     }
@@ -38,17 +39,25 @@ class TweetTableViewController: UITableViewController, UITextFieldDelegate {
     private var lastTwitterRequest: Twitter.Request?
     
     private func searchForTweets() {
-        if let request = twitterRequest() {
+        if let request = lastTwitterRequest?.newer ?? twitterRequest() {
             lastTwitterRequest = request
             request.fetchTweets { [weak self] newTweets in
-                if request == self?.lastTwitterRequest {
-                    self?.tweets.insert(newTweets, at: 0)
-                    DispatchQueue.main.async {
+                DispatchQueue.main.async {
+                    if request == self?.lastTwitterRequest {
+                        self?.tweets.insert(newTweets, at: 0)
                         self?.tableView.insertSections([0], with: .fade)
                     }
+                    self?.refreshControl?.endRefreshing()
                 }
             }
+        } else {
+            self.refreshControl?.endRefreshing()
         }
+    }
+    
+    
+    @IBAction func refresh(_ sender: UIRefreshControl) {
+        searchForTweets()
     }
     
     override func viewDidLoad() {
@@ -92,6 +101,10 @@ class TweetTableViewController: UITableViewController, UITextFieldDelegate {
         }
         
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return "\(tweets.count-section)"
     }
 
 }
