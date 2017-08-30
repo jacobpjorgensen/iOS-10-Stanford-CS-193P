02d97a536283819d580bc0c42a51b95034bed82b
Jacob Jorgensen <jacob.jorgensen@domo.com>
2017-08-30 08:58:57 -0600

    Added Lecture 13 code

---
+++ Lecture 13 - FaceIt/FaceIt/EmotionsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  EmotionsViewController.swift
+//  FaceIt
+//
+//  Created by Jacob Jorgensen on 6/8/17.
+//  Copyright © 2017 Domo, Inc. All rights reserved.
+//
+
+import UIKit
+
+class EmotionsViewController: VCLLoggingViewController {
+
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        var destinationViewController = segue.destination
+        if let navigationController = destinationViewController as? UINavigationController {
+            destinationViewController = navigationController.visibleViewController ?? destinationViewController
+        }
+        if let faceViewController = destinationViewController as? FaceViewController,
+            let identifier = segue.identifier,
+            let expression = emotionalFaces[identifier] {
+                faceViewController.expression = expression
+            faceViewController.navigationItem.title = (sender as? UIButton)?.currentTitle
+        }
+    }
+    
+    private let emotionalFaces: Dictionary<String, FacialExpression> = [
+        "sad" : FacialExpression(eyes: .closed, mouth: .frown),
+        "happy" : FacialExpression(eyes: .open, mouth: .smile),
+        "worried" : FacialExpression(eyes: .open, mouth: .smirk)
+    ]
+
+}
